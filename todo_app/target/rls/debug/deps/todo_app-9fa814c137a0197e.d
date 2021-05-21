1aa9606cdb1db15c7f79acfedb5210b7f098b410
henry <henry.zhang@hey.com>
2021-05-21 16:26:37 -0700

    setup structs

---
+++ todo_app/target/rls/debug/deps/todo_app-9fa814c137a0197e.d
@@ -0,0 +1,5 @@
+/Users/henry/.tmp/rust_web/todo_app/target/rls/debug/deps/todo_app-9fa814c137a0197e.rmeta: src/main.rs
+
+/Users/henry/.tmp/rust_web/todo_app/target/rls/debug/deps/todo_app-9fa814c137a0197e.d: src/main.rs
+
+src/main.rs:
---
+++ todo_app/target/rls/debug/deps/todo_app-b623de01fab6ae2d.d
@@ -0,0 +1,5 @@
+/Users/henry/.tmp/rust_web/todo_app/target/rls/debug/deps/todo_app-b623de01fab6ae2d.rmeta: src/main.rs
+
+/Users/henry/.tmp/rust_web/todo_app/target/rls/debug/deps/todo_app-b623de01fab6ae2d.d: src/main.rs
+
+src/main.rs:
