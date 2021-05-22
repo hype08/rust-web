9cdcfd71090cf305f1bf87fd4d8c0ad7f0320a89
henry <henry.zhang@hey.com>
2021-05-22 01:30:17 -0700

    collect args and write to JSON

--- todo_app/src/main.rs
+++ todo_app/src/main.rs
@@ -1,13 +1,14 @@
-mod to_do;
-use to_do::structs::traits::create::Create;
-use to_do::to_do_factory;
-use to_do::ItemTypes;
-
+mod state;
+use std::env;
+use state::{write_to_file, read_file};
+use serde_json::value::Value;
+use serde_json::{Map, json};
 fn main() {
-    let to_do_item: Result<ItemTypes, &'static str> = to_do_factory("pending", "make");
-
-    match to_do_item.unwrap() {
-        ItemTypes::Pending(item) => item.create(&item.super_struct.title),
-        ItemTypes::Done(item) => println!("it's done with title {}", item.super_struct.title),
-    }
+    let args: Vec<String> = env::args().collect();
+    let status: &String = &args[1];
+    let title: &String = &args[2];
+    let mut state: Map<String, Value> =        read_file(String::from("./state.json"));
+    println!("{:?}", state);
+    state.insert(title.to_string(), json!(status));
+    write_to_file("./state.json".to_string(), &mut state);
 }
--- todo_app/src/state.rs
+++ todo_app/src/state.rs
@@ -1,11 +1,21 @@
 use std::fs::File;
 use std::fs;
 use std::io::Read;
+
 use serde_json::Map;
 use serde_json::value::Value;
 use serde_json::json;
-pub fn read_file(file_name: &str) -> Map<String, Value> {
-    let mut file = File::open(file_name.to_string()).unwrap();
+
+
+/// This function reads a JSON file from disk.
+///
+/// # Arguments
+/// * file_name (String): the path to the file being read
+///
+/// # Returns
+/// (serde_json::value::Value): the values from the JSON file
+pub fn read_file(file_name: String) -> Map<String, Value> {
+    let mut file = File::open(file_name).unwrap();
     let mut data = String::new();
     file.read_to_string(&mut data).unwrap();
     let json: Value = serde_json::from_str(&data).unwrap();
@@ -13,7 +23,15 @@ pub fn read_file(file_name: &str) -> Map<String, Value> {
     return state
 }
 
-pub fn write_to_file(file_name: &str, state: &mut Map<String, Value>) {
-  let new_data = json!(state);
-  fs::write(file_name.to_string(), new_data.to_string()).expect("Unable to write file");
+/// This function writes a JSON map to file on disk.
+///
+/// # Arguments
+/// * file_name (String): the path to the file being read
+/// * state (Map<String, Value>): the data being written to disk
+///
+/// # Returns
+/// None
+pub fn write_to_file(file_name: String, state: &mut Map<String, Value>) {
+    let new_data = json!(state);
+    fs::write(file_name, new_data.to_string()).expect("Unable to write file");
 }
