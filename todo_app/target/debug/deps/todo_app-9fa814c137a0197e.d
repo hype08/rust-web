b46c582bf2f6269ff578754ab04abb2344a8348b
henry <henry.zhang@hey.com>
2021-05-21 20:51:14 -0700

    add traits

--- todo_app/target/debug/deps/todo_app-9fa814c137a0197e.d
+++
@@ -1,5 +0,0 @@
-/Users/henry/.tmp/rust_web/todo_app/target/debug/deps/todo_app-9fa814c137a0197e.rmeta: src/main.rs
-
-/Users/henry/.tmp/rust_web/todo_app/target/debug/deps/todo_app-9fa814c137a0197e.d: src/main.rs
-
-src/main.rs:
--- todo_app/target/debug/deps/todo_app-b623de01fab6ae2d.d
+++
@@ -1,5 +0,0 @@
-/Users/henry/.tmp/rust_web/todo_app/target/debug/deps/todo_app-b623de01fab6ae2d.rmeta: src/main.rs
-
-/Users/henry/.tmp/rust_web/todo_app/target/debug/deps/todo_app-b623de01fab6ae2d.d: src/main.rs
-
-src/main.rs:
