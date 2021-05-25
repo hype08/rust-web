d1c655d56e1f1ba5e751fdbb79948f9fe790e6da
henry <henry.zhang@hey.com>
2021-05-25 09:33:29 -0700

    import views factory and call in main

---
+++ managing_views/src/views/auth/mod.rs
@@ -0,0 +1,18 @@
+use actix_web::web;
+mod login;
+mod logout;
+
+// if we use super in a mod.rs, we import files in the parent directory mod.rs file.
+use super::path::Path;
+
+// instead of passing actix_web::App, we pass ServiceConfig struct to define routes.
+pub fn auth_factory(app: &mut web::ServiceConfig) {
+  let base_path: Path = Path{prefix: String::from("/auth")};
+
+  app
+    .route(&base_path.define(String::from("/login")),
+      web::get().to(login::login))
+    .route(&base_path.define(String::from("/logout")),
+      web::get().to(login::login))
+
+}
