4139f9a295496c4703869212d31d49025ee82a64
henry <henry.zhang@hey.com>
2021-05-29 13:09:56 -0700

    auth factory

--- managing_views/src/views/auth/mod.rs
+++ managing_views/src/views/auth/mod.rs
@@ -1,22 +1,19 @@
 use actix_web::web;
 mod login;
 mod logout;
-
-// if we use super in a mod.rs, we import files in the parent directory mod.rs file.
 use super::path::Path;
-
-// instead of passing actix_web::App, we pass ServiceConfig struct to define routes.
-pub fn auth_factory(app: &mut web::ServiceConfig) {
+pub fn auth_factory(app: &mut web::ServiceConfig, logout: bool) {
     let base_path: Path = Path {
         prefix: String::from("/auth"),
     };
-
-    app.route(
-        &base_path.define(String::from("/login")),
+    let app = app.route(
+        &base_path.define(String::from("/        login")),
         web::get().to(login::login),
-    )
-    .route(
-        &base_path.define(String::from("/logout")),
-        web::get().to(login::login),
-    )
+    );
+    if logout {
+        app.route(
+            &base_path.define(String::from("/logout")),
+            web::get().to(logout::logout),
+        );
+    }
 }
--- managing_views/src/views/mod.rs
+++ managing_views/src/views/mod.rs
@@ -1,6 +1,15 @@
 use actix_web::web;
 mod auth;
 mod path;
+use std::env;
 pub fn views_factory(app: &mut web::ServiceConfig) {
-    auth::auth_factory(app);
+    let args: Vec<String> = env::args().collect();
+    let param: &String = &args[args.len() - 1];
+    if param.as_str() == "cancel_logout" {
+        println!("logout view isn't being configured");
+        auth::auth_factory(app, false);
+    } else {
+        println!("logout view is being configured");
+        auth::auth_factory(app, true);
+    }
 }
