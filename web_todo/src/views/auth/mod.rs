3158776ad934586ee130f3044899447e7456c495
henry <henry.zhang@hey.com>
2021-06-06 01:06:29 -0700

    auths/mod and views/mod

--- web_todo/src/views/auth/mod.rs
+++ web_todo/src/views/auth/mod.rs
@@ -2,18 +2,17 @@ use actix_web::web;
 mod login;
 mod logout;
 use super::path::Path;
-pub fn auth_factory(app: &mut web::ServiceConfig, logout: bool) {
+pub fn auth_factory(app: &mut web::ServiceConfig) {
     let base_path: Path = Path {
         prefix: String::from("/auth"),
     };
-    let app = app.route(
-        &base_path.define(String::from("/        login")),
-        web::get().to(login::login),
-    );
-    if logout {
-        app.route(
+    let app = app
+        .route(
+            &base_path.define(String::from("/login")),
+            web::get().to(login::login),
+        )
+        .route(
             &base_path.define(String::from("/logout")),
             web::get().to(logout::logout),
         );
-    }
 }
--- web_todo/src/views/to_do/mod.rs
+++ web_todo/src/views/to_do/mod.rs
@@ -2,7 +2,11 @@ use actix_web::web;
 mod create;
 use super::path::Path;
 pub fn item_factory(app: &mut web::ServiceConfig) {
-    let base_path: Path = Path{prefix: String::from("/item")};
-    app.route(&base_path.define(        String::from("/create/{title}")),
-            web::get().to(create::create));
+    let base_path: Path = Path {
+        prefix: String::from("/item"),
+    };
+    app.route(
+        &base_path.define(String::from("/create/{title}")),
+        web::get().to(create::create),
+    );
 }
